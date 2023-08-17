888ff97a1546d58065a920efb96a587df12aa3de
Joseph Kihurani <joseph.kihurani@student.moringaschool.com>
2023-08-17 12:38:54 +0300

    initial gender button selector learning sample

---
+++ scripts.js
@@ -0,0 +1,9 @@
+let btnShow=document.querySelector('button');
+let result = document.querySelector('h1');
+
+btnShow.addEventListener('click',() =>{
+    let selected = document.querySelector('input[type="radio"]:checked');
+    result.innerText = selected.value;
+    result.innerText = selected.parentElement.textContent;
+    alert(result.innerText)
+})
